feadcc15f96fb08c90e482b541a72639879157e3
afsar594 <malikafsar594@gmail.com>
2023-07-18 11:41:51 -0700

    fileupload

---
+++ src/app/admin/admin-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+
+
+const routes: Routes = [
+  {path:'fileUpload',component:FileUploadComponent},
+  {path:'fileUpload/:id',component:FileUploadComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdminRoutingModule { }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { AdminRoutingModule } from './admin-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FileUploadComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutingModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
